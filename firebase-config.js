5d14f00c8d74b6ff01d2b569797b740d0ce6fd24
LJ974 <jeanne.lop974@gmail.com>
2026-07-11 20:33:23 +0300

    Ajoute la configuration Firebase

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_6uq5HT2Ax0dGiSoIPz8umwVid1M65ck",
+  authDomain: "chaudron-collectif.firebaseapp.com",
+  databaseURL: "https://chaudron-collectif-default-rtdb.firebaseio.com",
+  projectId: "chaudron-collectif",
+  storageBucket: "chaudron-collectif.appspot.com"
+};
